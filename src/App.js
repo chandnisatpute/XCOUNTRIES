a907b8ce0e28f16b224113014f049dde10272b17
chandnisatpute <chandnisatpue@gmail.com>
2025-04-03 06:19:30 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import  Countries from "./components/Countries";
+
+
+
+function App() {
+    return (
+        <div>
+            <Countries/>
+        </div>
+    );
+}
+
+
+
+export default App;
---
+++ src/components/Countries.jsx
@@ -0,0 +1,66 @@
+import React, {useEffect, useState} from "react";
+
+
+
+const Card = ({name, flag}) => {
+    return (
+        <div
+        style={{
+            display:"flex",
+            flexDirection:"column",
+            gap:"4px",
+            justifyContent:"center",
+            alignItems:"center",
+            height:"200px",
+            width:"200px",
+            border:"0.5px solid black",
+            borderRadius:"5px",
+            padding:"10px",
+            textAlign:"center"
+        }}
+        >
+            <img
+            src={flag}
+            alt={`Flag of ${name}`}
+            style={{
+                width:"100px",
+                height:"100px",
+            }}
+            />
+        </div>
+    );
+};
+
+const API_ENDPOINT = " https://xcountries-backend.azurewebsites.net/all";
+export default function Countries(){
+    const[countries,setCountries]=useState([]);
+
+    useEffect(() => {
+        const fetchData= async () => {
+            try{
+            const res= await fetch(API_ENDPOINT);
+            const jsonRes= await res.json();
+            setCountries(jsonRes);
+        }catch(error){
+            console.error( "Error fetching data: "+error);
+        }
+        };
+
+        fetchData();
+    }, []);
+
+    return (
+        <div
+        style={{
+            display:"flex",
+            flexWrap:"wrap",
+            gap:"10px",
+        }}
+        >
+            {countries.map(({name, flag, abbr}) => (
+                <Card name={name} flag={flag} key={abbr}/>
+            ))}
+        </div>
+
+    );
+}
